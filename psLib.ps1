88a50407a6baf231605b314fc1e5b66933ac344b
linhb <vipyouxiang7777@163.com>
2018-05-26 10:52:16 +0800

    add comment

--- psLib.ps1
+++ psLib.ps1
@@ -69,6 +69,7 @@ enum ProgramEnum {
 # Function
 
 function he {
+    # Examples and Alias
     help $args[0] -Examples
     $ErrorActionPreference = "SilentlyContinue"
     if ($args[0] -match "-") {
